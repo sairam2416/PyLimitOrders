f9aee60733a133b3d9fd35f907a2959cd744b015
sairam2416 <165991729+sairam2416@users.noreply.github.com>
2024-04-08 10:22:39 +0530

    Added more test cases  for other products having different amount and price limits in limit_order_agent_tests.py

--- limit_order_agent_tests.py
+++ limit_order_agent_tests.py
@@ -32,6 +32,41 @@ class LimitOrderAgentTest(unittest.TestCase):
         self.assertTrue(self.limit_order_agent.orders)
         print("test_execute_sell_order Passed Successfully")
 
+        def test_retry_held_orders_multiple_products(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100.5)
+        self.limit_order_agent.add_order('buy', 'GOOG', 2000, 1200.0)
+        self.limit_order_agent.on_price_tick('IBM', 101.0)
+        self.limit_order_agent.on_price_tick('GOOG', 1150.0)
+        self.assertTrue(self.limit_order_agent.orders)
+        self.limit_order_agent.on_price_tick('IBM', 99.0)
+        self.limit_order_agent.on_price_tick('GOOG', 1050.0)
+        self.assertFalse(self.limit_order_agent.orders)
+        print("test_retry_held_orders_multiple_products Passed Successfully")
+
+    def test_execute_orders_multiple_products(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100.5)
+        self.limit_order_agent.add_order('sell', 'GOOG', 2000, 1150.0)
+        self.limit_order_agent.on_price_tick('IBM', 99.0)
+        self.limit_order_agent.on_price_tick('GOOG', 1200.0)
+        self.assertFalse(self.limit_order_agent.orders)
+        print("test_execute_orders_multiple_products Passed Successfully")
+
+    def test_retry_held_orders_multiple_amounts(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100.5)
+        self.limit_order_agent.add_order('buy', 'IBM', 2000, 99.5)
+        self.limit_order_agent.on_price_tick('IBM', 101.0)
+        self.assertTrue(self.limit_order_agent.orders)
+        self.limit_order_agent.on_price_tick('IBM', 98.0)
+        self.assertFalse(self.limit_order_agent.orders)
+        print("test_retry_held_orders_multiple_amounts Passed Successfully")
+
+    def test_execute_orders_multiple_amounts(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100.5)
+        self.limit_order_agent.add_order('buy', 'IBM', 2000, 99.5)
+        self.limit_order_agent.on_price_tick('IBM', 97.0)
+        self.assertFalse(self.limit_order_agent.orders)
+        print("test_execute_orders_multiple_amounts Passed Successfully")
+    
 
 if __name__ == '__main__':
     unittest.main()
